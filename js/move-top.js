a9c746b6e39822b92347e6578f257369108ff0cf
himanshujain71 <himanshujain71@gmail.com>
2017-10-17 00:13:23 +0530

    Initial commi22t

--- js/move-top.js
+++ js/move-top.js
@@ -3,3 +3,19 @@
 if(sd>settings.min)
 $(containerIDhash).fadeIn(settings.inDelay);else
 $(containerIDhash).fadeOut(settings.Outdelay);});};})(jQuery);
+
+$(document).ready(function(){
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+    
+    if (scrollTop >= 400) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 400) {
+      $('#global-nav').removeClass('scrolled-nav');
+    } 
+    
+  }); 
+  
+});
